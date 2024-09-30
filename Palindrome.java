24b2b91279d4cac9db8c53df07513af21bf7ae23
dashainCodes <dashain2.07@gmail.com>
2024-09-30 10:15:14 +0545

    two sum and palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -1,8 +1,9 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
 public class Palindrome {
 
     static boolean detectPalindrome(int a) {
-String b=Integer.toString(a);
+        String b = Integer.toString(a);
         for (int i = 0; i <= b.length() / 2; i++) {
             int len = b.length() - 1;
             if (b.charAt(i) != b.charAt(len - i)) {
@@ -13,10 +14,10 @@ String b=Integer.toString(a);
     }
 
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
         System.out.println("Enter no to check");
-    
-        int no = myObj.nextInt(); 
+
+        int no = myObj.nextInt();
         System.out.print(detectPalindrome(no));
     }
 }
